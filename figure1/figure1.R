d833e71c7a8af746322dc3768bee167bcc0edc67
JSILVEN <j.j.m.silven@uva.nl>
2020-02-13 16:49:33 +0100

    added geom_line

--- figure1/figure1.R
+++ figure1/figure1.R
@@ -13,7 +13,7 @@ ggplot(data = data, aes(x = genotype, y = weight, color = genotype)) +
   theme_bw()
 
 ggplot(data = data, aes(x = genotype, y = weight, color = genotype)) +
-  facet_wrap(~day, labeller = label_both) +
+  facet_grid(~day, labeller = label_both) +
   geom_boxplot() +
   labs(x = "Genotype CP",
        y = "Weight larvae (grams)",
@@ -21,5 +21,12 @@ ggplot(data = data, aes(x = genotype, y = weight, color = genotype)) +
   geom_point((aes(color=genotype)), position=position_jitterdodge(), alpha=0.6, size=0.8) +
   theme_bw()
 
+ggplot(data = data, aes(x= day, y = weight, color = genotype)) +
+  geom_line(aes(group = interaction(genotype, replicate), color = genotype)) +
+  facet_grid(~block, labeller = label_both) +
+  labs(x = "Age caterpillars (days)",
+       y = "Weight larvae (grams)",
+       title = "Weight WT and KO m. sexta larvae feeding on N. attenuata plants") +
+  theme_bw()
 
 
